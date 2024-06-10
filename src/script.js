6e96ec27f9e7a32546cd0f07cb3ebac929d0c498
LouV22 <Lourenco.simas.velez@gmail.com>
2024-06-09 23:48:56 -0400

    Added Nav, Some buttons, WIP

---
+++ src/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+// Social Media Functions
+
+// Open Facebook
+function redirectFacebook(){
+    window.open('https://www.facebook.com/Lourenco1234', '_blank');
+}
+
+// Open Instagram
+function redirectInstagram(){
+    window.open('https://www.instagram.com/lourenco.velez/', '_blank');
+}
